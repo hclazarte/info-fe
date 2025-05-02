3741535575c524f3711aaa24565957688cc2ab17
Lazarte, Hector <hlazarte@everfi.com>
2025-05-02 17:14:22 -0400

    poner secciones en las tarjetas 2

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -15,8 +15,8 @@ const Tarjeta = ({ comercio, onClick }) => {
     : 'bg-white'
 
   return (
-    <div onClick={onClick} className={`${tarjetaBase} ${fondo}`}>
-      <section className='px-6 py-4'>
+    <section onClick={onClick} className={`${tarjetaBase} ${fondo}`}>
+      <div className='px-6 py-4'>
         {/* Nombre de la Empresa */}
         <div className='font-bold text-xl mb-2 flex items-center gap-2'>
           {comercio.empresa}
@@ -50,8 +50,8 @@ const Tarjeta = ({ comercio, onClick }) => {
           {comercio.telefono2 ? `, ${comercio.telefono2}` : ''}
           {comercio.telefono_whatsapp ? `, ${comercio.telefono_whatsapp}` : ''}
         </p>
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
 
