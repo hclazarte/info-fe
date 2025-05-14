468e483ec08a7c3d0cba87d0f9fda141f0a7dcfb
Lazarte, Hector <hlazarte@everfi.com>
2025-05-14 12:24:11 -0400

    añadir timeout

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,8 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 60_000,             
+  expect: { timeout: 8000 },   
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
