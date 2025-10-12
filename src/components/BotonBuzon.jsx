e2c2787dc75268fc545809354d1cdd0bcdd639fd
Lazarte, Hector <hlazarte@everfi.com>
2025-10-12 12:46:48 -0400

    Arreglar descripcion alterna

--- src/components/BotonBuzon.jsx
+++ src/components/BotonBuzon.jsx
@@ -6,7 +6,12 @@ export default function BotonBuzon({ onClick }) {
       onClick={onClick}
       className='w-20 aspect-square flex flex-col items-center justify-center bg-inf8 text-white rounded-md hover:bg-inf3'
     >
-      <img src={IconoCorreo} alt='Correo' width={40} height={40} />
+      <img
+        src={IconoCorreo}
+        alt='Buzón de Sugerencias'
+        width={40}
+        height={40}
+      />
       <span className='text-xs leading-none mt-1'>Contactar Infomóvil</span>
     </button>
   )
